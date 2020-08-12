7fcdaba104afc78c467b1a5609b25d9f56bbcfa3
TimoL <timo.lange98@aol.de>
2020-08-12 14:47:11 +0200

    Reset function

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var head;
+var snake;
 var dir;
 var size;
 
@@ -12,16 +12,15 @@ function setup() {
     frameRate(10);
 
     size = scl;
-    head = new Snake(15 * scl, 15 * scl, size);
-    last = head;
+    snake = new Snake(15 * scl, 15 * scl, size);
 }
 
 function draw() {
     background(51);
-    head.show();
-    head.setDir(dir);
-    head.move();
-    head.death();
+    snake.show();
+    snake.setDir(dir);
+    snake.move();
+    snake.death();
 
     if (eaten) {
         block = randomBlock();
@@ -29,7 +28,7 @@ function draw() {
     }
     block.show();
 
-    if (head.eat(block))
+    if (snake.eat(block))
         eaten = true;
 }
 
@@ -57,5 +56,16 @@ function keyPressed() {
         case LEFT_ARROW:
             dir = dir === 1 ? dir : 3;
             break;
+
+        case ESCAPE:
+            reset();
+            break;
     }
+}
+
+function reset(){
+    snake = new Snake(15 * scl, 15 * scl, size);
+    dir = null;
+    block = null;
+    eaten = true;
 }
